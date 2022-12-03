a24b37ae3d7fafc79a922b437534b24fbf99c90f
Finn Le Sueur <finn@lesueur.nz>
2022-12-03 20:43:25 +1300

    Fix typo

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-cd /srv/rss.lesueur/nz
+cd /srv/rss.lesueur.nz/
 source venv/bin/activate
 git pull origin main
 ./main.py
