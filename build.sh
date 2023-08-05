bdf4ce6dec84dce35f3a0fb6ad13482c0bd59517
Finn LeSueur <finn@lesueur.nz>
2023-08-05 13:24:05 +1200

    Use Synology directory

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-cd /srv/rss.lesueur.nz/
+#cd /srv/rss.lesueur.nz/
+cd /volume1/web/rss.lesueur.nz/
 source venv/bin/activate
 git pull origin main
 ./main.py
